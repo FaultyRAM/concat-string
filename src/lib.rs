68fee1275d449d25900f20c8b72be999c21602ae
FaultyRAM <explodingpsu@gmail.com>
2017-09-28 14:34:12 +1000

    `concat_string!`

--- src/lib.rs
+++ src/lib.rs
@@ -27,3 +27,57 @@
 #![forbid(unused_qualifications)]
 #![forbid(unused_results)]
 #![forbid(variant_size_differences)]
+
+#[macro_export]
+/// Concatenates a series of string slices into an owned string.
+///
+/// This macro accepts zero or more arguments, where each argument implements `AsRef<str>`, and
+/// efficiently combines their string representations into a `String` in order of declaration.
+///
+/// This is mainly useful for cases where the cost of parsing a format string outweighs the cost
+/// of converting its arguments. Because `concat_string` avoids format strings entirely, it can
+/// achieve a higher level of performance than using `format!` or other formatting utilities that
+/// return a `String`.
+///
+/// # Example
+///
+/// ```rust
+/// #[macro_use(concat_string)]
+/// extern crate concat_string;
+///
+/// fn main() {
+///     println!("{}", concat_string!("Hello", String::from(" "), "world"));
+/// }
+/// ```
+macro_rules! concat_string {
+    () => { String::with_capacity(0) };
+    ($($s:expr),+) => {{
+        use std::ops::AddAssign;
+        let mut len = 0;
+        $(len.add_assign(AsRef::<str>::as_ref(&$s).len());)+
+        let mut buf = String::with_capacity(len);
+        $(buf.push_str($s.as_ref());)+
+        buf
+    }};
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn concat_string_0_args() {
+        let s = concat_string!();
+        assert_eq!(s, String::from(""));
+    }
+
+    #[test]
+    fn concat_string_1_arg() {
+        let s = concat_string!("foo");
+        assert_eq!(s, String::from("foo"));
+    }
+
+    #[test]
+    fn concat_string_str_string() {
+        let s = concat_string!("foo", String::from("bar"));
+        assert_eq!(s, String::from("foobar"));
+    }
+}
