8f237752877fcf887915d0d63d3f215dfa071b03
FaultyRAM <explodingpsu@gmail.com>
2017-09-28 13:38:28 +1000

    Initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# concat-string
+
+[![Travis](https://img.shields.io/travis/FaultyRAM/concat-string.svg)][1]
+[![AppVeyor](https://img.shields.io/appveyor/ci/FaultyRAM/concat-string.svg)][2]
+[![Crates.io](https://img.shields.io/crates/v/concat-string.svg)][3]
+[![Docs.rs](https://docs.rs/concat-string/badge.svg)][4]
+
+Provides macros for concatenating string slices into owned strings.
+
+## License
+
+Licensed under either of
+
+* Apache License, Version 2.0,
+  ([LICENSE-APACHE](LICENSE-APACHE) or http://www.apache.org/licenses/LICENSE-2.0)
+* MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+
+at your option.
+
+### Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally
+submitted for inclusion in the work by you, as defined in the Apache-2.0
+license, shall be dual licensed as above, without any additional terms or
+conditions.
+
+[1]: https://travis-ci.org/FaultyRAM/concat-string
+[2]: https://ci.appveyor.com/project/FaultyRAM/concat-string
+[3]: https://crates.io/crates/concat-string
+[4]: https://docs.rs/concat-string
---
+++ src/lib.rs
@@ -0,0 +1,29 @@
+// Copyright (c) 2017 FaultyRAM
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be copied, modified, or
+// distributed except according to those terms.
+
+//! Macros for concatenating string slices into owned strings.
+
+#![cfg_attr(feature = "clippy", feature(plugin))]
+#![cfg_attr(feature = "clippy", plugin(clippy))]
+#![cfg_attr(feature = "clippy", forbid(clippy))]
+#![cfg_attr(feature = "clippy", forbid(clippy_internal))]
+#![cfg_attr(feature = "clippy", forbid(clippy_pedantic))]
+#![cfg_attr(feature = "clippy", forbid(clippy_restrictions))]
+#![forbid(warnings)]
+#![forbid(anonymous_parameters)]
+#![forbid(box_pointers)]
+#![forbid(fat_ptr_transmutes)]
+#![forbid(missing_copy_implementations)]
+#![forbid(missing_debug_implementations)]
+#![forbid(missing_docs)]
+#![forbid(trivial_casts)]
+#![forbid(trivial_numeric_casts)]
+#![forbid(unsafe_code)]
+#![forbid(unused_import_braces)]
+#![forbid(unused_qualifications)]
+#![forbid(unused_results)]
+#![forbid(variant_size_differences)]
